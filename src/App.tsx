dc0e691d167e7ee09f71aebda3bef439269f151b
BuraYu <burak.yueksel@hotmail.com>
2025-06-12 20:45:59 +0200

    feat: add ThemeContext for darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    // TODO add theme provider
-    <div>
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <ThemeProvider>
+      <div>
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useTheme } from "../contexts/ThemeContext";
 
 const Header: React.FunctionComponent<{}> = () => {
+  const { isDarkMode, toggleDarkMode } = useTheme();
+
   return (
     <div>
-      <h1>Header</h1>
+      <h1>Darkmode test</h1>
+      <button onClick={toggleDarkMode}>Change Mode</button>
+      {isDarkMode ? (
+        <h1 className="bg-amber-300">Bright</h1>
+      ) : (
+        <h1 className="bg-blue-900">Dark</h1>
+      )}
     </div>
   );
 };
