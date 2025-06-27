0fd8f3fe3aa4876492776b4ba97f7435960659d4
BuraYu <burak.yueksel@hotmail.com>
2025-06-28 00:42:57 +0200

    feat: add analytis for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import "./i18.tsx";
 import { BrowserRouter } from "react-router";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
